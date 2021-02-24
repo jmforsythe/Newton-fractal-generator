50cc26c2f458da28db4e206445c5da93ef1ff1c8
Jonathan <u1803235@live.warwick.ac.uk>
2021-02-24 00:05:53 +0000

    Removed the hardcoding of some variables, replaced them with default values that will be able to be changed by command line arguments in the future

--- main.cpp
+++ main.cpp
@@ -2,47 +2,76 @@
 #include <fstream>
 #include <regex>
 #include <string>
+
 #include "newtonfractal.hpp"
 
 int main(int argc, char* argv[]) {
-	std::vector<Complex> coeff;
-	std::regex expression("(.*)([+-].*)i");
-	for (int i=1; i<argc; i++) {
-		std::smatch match;
-		std::string str(argv[i]);
-		std::regex_match(str, match, expression);
-		switch (match.size()) {
-			case 2:
-				coeff.push_back(Complex(std::stod(match[1]))); break;
-			case 3:
-				coeff.push_back(Complex(std::stod(match.str(1)), std::stod(match.str(2)))); break;
-			default:
-				coeff.push_back(Complex()); break;
+	
+	// Default NewtonFractal params
+	struct NFParams nfp;
+	nfp.p = Polynomial({Complex(-1), Complex(), Complex(), Complex(1)});
+	nfp.origin = Complex(-1.0, 1.0);
+	nfp.pixel_height = 1024;
+	nfp.pixel_width = 1024;
+	nfp.width = 2.0;
+	nfp.height = 2.0;
+	nfp.num_colours = 5;
+	nfp.palette = {Colour(255,0,0), Colour(0,255,0), Colour(0,0,255), Colour(0,255,255), Colour(255,0,255)};
+	nfp.colour_iterations = true;
+	nfp.max_iter_depth = 4000;
+	nfp.verbose = false;
+	
+	// Add command line arguments parsing
+	// -v: verbose, give progress updates and display polynomial
+	// -c [coefficients]: coefficients in ascending order
+	// -x[i] next argument is x^i coefficient, incompatible with -c
+	// -pw [int]: nfp.pixel_width
+	// -ph [int]: nfp.pixel_height
+	// -o [a+bi]: nfp.origin
+	// -cw [double]: nfp.width
+	// -ch [double]: nfp.height
+	// -p [filename]: path to file containing colour palette
+	// --noshading: nfp.colour_iterations = false
+	// --maxdepth [int]: max_iter_depth
+	
+	if (argc>1) {
+		std::vector<Complex> coeff;
+		std::regex expression("(.*)([+-].*)i");
+		for (int i=1; i<argc; i++) {
+			std::smatch match;
+			std::string str(argv[i]);
+			std::regex_match(str, match, expression);
+			switch (match.size()) {
+				case 2:
+					coeff.push_back(Complex(std::stod(match[1]))); break;
+				case 3:
+					coeff.push_back(Complex(std::stod(match.str(1)), std::stod(match.str(2)))); break;
+				default:
+					coeff.push_back(Complex()); break;
+			}
 		}
+		Polynomial p(coeff);
+		if (nfp.verbose) std::cout << p << std::endl;
+		if (p.getDegree() == 0) {
+			std::cout << "Cannot perform process on constant polynomial" << std::endl;
+			return 0;
+		}
+		nfp.p = p;
 	}
-	Polynomial p(coeff);
-	std::cout << p << std::endl;
-	if (p.getDegree() == 0) {
-		std::cout << "Cannot perform process on constant polynomial" << std::endl;
-		return 0;
-	}
-	Complex origin(-1.0, 1.0);
-	double width = 2.0;
-	double height = 2.0;
 	
-	NewtonFractal nf(p, origin, width, height);
-	Colour *fractal_colour_array = nf.createFractal(true);
+	NewtonFractal nf(nfp);
+	Colour *fractal_colour_array = nf.createFractal();
 	
-	std::string filename = p.toString() + ".ppm";
+	std::string filename = nfp.p.toString() + ".ppm";
 	
 	std::ofstream ImageFile(filename);
 	
-	ImageFile << "P3 " << NewtonFractal::NUMPIXELSWIDTH << " " << NewtonFractal::NUMPIXELSHEIGHT << " " << 255 << std::endl;
+	ImageFile << "P3 " << nfp.pixel_width << " " << nfp.pixel_height << " " << 255 << std::endl;
 	
-	for (int i=0; i<NewtonFractal::NUMPIXELSHEIGHT*NewtonFractal::NUMPIXELSWIDTH; i++) {
+	for (int i=0; i<nfp.pixel_height*nfp.pixel_width; i++) {
 		auto c = fractal_colour_array[i];
 		ImageFile << (int)c._r << " " << (int)c._g << " " << (int)c._b << " ";
-		if (i%NewtonFractal::NUMPIXELSWIDTH == 0) {
+		if (i%nfp.pixel_width == 0) {
 			ImageFile << std::endl;
 		}
 	}
--- newtonfractal.hpp
+++ newtonfractal.hpp
@@ -10,6 +10,19 @@
 
 #include "colour.hpp"
 
+struct NFParams {
+	Polynomial p;
+	Complex origin; // top left of image
+	int pixel_height, pixel_width;
+	double width, height;
+	bool colour_iterations;
+	int max_iter_depth;
+	bool verbose;
+	size_t num_colours;
+	std::vector<Colour> palette;
+};
+
+
 class NewtonFractal {
 private:
 	Complex _origin;
@@ -18,13 +31,15 @@ private:
 	Newton _iterator;
 	std::vector<Complex> _roots;
 		
-	bool _colorIterations;
+	bool _colorIterations, _verbose;
 	
-public:
-	static const int NUMPIXELSWIDTH = 512;
-	static const int NUMPIXELSHEIGHT = 512;
+	int _pixel_width, _pixel_height;
 	
-	NewtonFractal(Polynomial &p, Complex &origin, double width, double height);
+	std::vector<Colour> _palette;
+	size_t _num_colours;
+	
+public:	
+	NewtonFractal(NFParams &nfp);
 	
 	void printRoots();
 	
@@ -32,7 +47,7 @@ public:
 	
 	Complex pixelToComplex(int i, int j);
 	
-	Colour * createFractal(bool colorIterations);
+	Colour * createFractal();
 };
 
 #endif
